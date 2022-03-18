1020f443aedb7f499f1a90f76f9cd2552679035b
yongqiangzheng <823022895@qq.com>
2022-03-18 12:08:35 +0800

    update

--- preprocess.py
+++ preprocess.py
@@ -3,16 +3,17 @@
 '''
 @File    :   preprocess.py
 @Time    :   2022/03/15 14:53:47
-@Author  :   ZYQ 
+@Author  :   ZYQ
 '''
 
 import os
 
 from nltk.corpus import sentiwordnet as swn
 from senticnet.senticnet import SenticNet
+from torch import le
 
 root = './Lexicon/'
-lexicon = ['SentiWordNet', 'SenticNet', 'Liu Bing']
+lexicon = ['Hu&Liu', 'SenticNet', 'SentiWordNet', 'VADER']
 pos_txt = 'positive.txt'
 neg_txt = 'negative.txt'
 if not os.path.exists(root):
@@ -23,14 +24,30 @@ for l in lexicon:
 
 
 def prepocess(lexicon):
-    path = root+lexicon+'/{}.txt'.format(lexicon.lower())
     pos_path = root+lexicon+'/'+pos_txt
     neg_path = root+lexicon+'/'+neg_txt
-    fout = open(path, 'w')
     fpos = open(pos_path, 'w')
     fneg = open(neg_path, 'w')
 
-    if lexicon == 'SentiWordNet':
+    if lexicon == 'Hu&Liu':
+        with open('./Source/Hu&Liu/positive-words.txt', 'r') as f1:
+            lines = f1.readlines()
+            for i in lines[35:]:
+                fpos.write(i)
+        with open('./Source/Hu&Liu/negative-words.txt', 'r') as f2:
+            lines = f2.readlines()
+            for i in lines[35:]:
+                fneg.write(i)
+    elif lexicon == 'SenticNet':
+        sn = SenticNet()
+        for k, v in sn.data.items():
+            if v[6] == 'positive':
+                fpos.write(k+'\t'+v[7]+'\n')
+            else:
+                fneg.write(k+'\t'+v[7]+'\n')
+    elif lexicon == 'SentiWordNet':
+        path = root+lexicon+'/{}.txt'.format(lexicon.lower())
+        fout = open(path, 'w')
         for i in swn.all_senti_synsets():
             if i.pos_score()+i.neg_score() > i.obj_score():
                 fout.write(str(i.synset)[8:-2]+'\t' +
@@ -41,21 +58,24 @@ def prepocess(lexicon):
                 elif i.pos_score() == 0.:
                     fneg.write(str(i.synset)[8:-2] +
                                '\t'+str(i.neg_score())+'\n')
-    elif lexicon == 'SenticNet':
-        sn = SenticNet()
-        for k, v in sn.data.items():
-            fout.write(k+'\t'+v[6]+'\t' + v[7]+'\n')
-            if v[6] == 'positive':
-                fpos.write(k+'\t'+v[7]+'\n')
-            else:
-                fneg.write(k+'\t'+v[7]+'\n')
+        fout.close
+    elif lexicon == 'VADER':
+        with open('./Source/VADER/vader_lexicon.txt', 'r') as f1:
+            lines = f1.readlines()
+            for i in lines:
+                word, score, _, _ = i.strip().split('\t')
+                if float(score) > 0.0:
+                    fpos.write(word+'\t'+score+'\n')
+                elif float(score) < 0.0:
+                    fneg.write(word+'\t'+score+'\n')
 
     print('done!')
-    fout.close
     fpos.close()
     fneg.close()
 
 
 if __name__ == '__main__':
+    # prepocess('Hu&Liu')
     # prepocess('SentiWordNet')
     # prepocess('SenticNet')
+    # prepocess('VADER')
